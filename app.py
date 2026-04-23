0b72eb1e44225fab83616d2dd9ae8ff9be1cf289
juniorpawar <yuvrajsingh3641@gmail.com>
2026-04-23 23:49:32 +0530

    Added render API Live URL for backend

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommendation-nlp-dl.onrender.com/" or "http://127.0.0.1:8000"
 
 st.set_page_config(page_title="CineMatch - Movie Recommender", page_icon="🎬", layout="wide")
 
